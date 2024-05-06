e66f9f01c6a1f3cee2b6af29bcf75c51caa92d3d
Jovincia <assogbadanielle15@gmail.com>
2024-05-06 23:53:30 +0100

    adding appbar and drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:drawer/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MainColors.brownCoffeeMilk,
+      ),
+      drawer: Drawer(
+        
+      ),
+    );
   }
 }
